4910e7b252eda7613b5883121e5396df1077a65b
Sinto Sajeev <sinto.s@aot-technologies.com>
2025-02-27 03:04:17 -0800

    Updated telemetry.py

--- forms-flow-api-utils/src/formsflow_api_utils/utils/telemetry.py
+++ forms-flow-api-utils/src/formsflow_api_utils/utils/telemetry.py
@@ -6,14 +6,11 @@ from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
     OTLPSpanExporter,
 )
-from opentelemetry.exporter.zipkin.json import ZipkinExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from opentelemetry.instrumentation.requests import RequestsInstrumentor
 
-def setup_tracing(app, service_name):
+def setup_tracing(service_name):
     """Sets up OpenTelemetry tracing for the application."""
     # Check if OpenTelemetry tracing is enabled
     if os.getenv("ENABLE_OPENTELEMETRY", "false").lower() != "true":
@@ -24,25 +21,10 @@ def setup_tracing(app, service_name):
     trace_provider = TracerProvider(resource=resource)
     trace.set_tracer_provider(trace_provider)
 
-    # Jaeger Exporter (OTLP)
-    jaeger_endpoint = os.getenv("JAEGER_ENDPOINT")
-    if jaeger_endpoint:
-        jaeger_exporter = OTLPSpanExporter(endpoint=jaeger_endpoint, insecure=True)
-        jaeger_span_processor = BatchSpanProcessor(jaeger_exporter)
-        trace_provider.add_span_processor(jaeger_span_processor)
-    else:
-        print("Jaeger endpoint not configured. Jaeger tracing will not be enabled.")
+    # Configure the OTLP exporter to send data to the OpenTelemetry Collector
+    otlp_endpoint = os.getenv("OTLP_ENDPOINT", "http://localhost:4317")
+    otlp_exporter = OTLPSpanExporter(endpoint=otlp_endpoint, insecure=True)
 
-    # Zipkin Exporter (HTTP)
-    zipkin_endpoint = os.getenv("ZIPKIN_ENDPOINT")
-    if zipkin_endpoint:
-        zipkin_exporter = ZipkinExporter(endpoint=zipkin_endpoint)
-        zipkin_span_processor = BatchSpanProcessor(zipkin_exporter)
-        trace_provider.add_span_processor(zipkin_span_processor)
-    else:
-        print("Zipkin endpoint not configured. Zipkin tracing will not be enabled.")
-
-    print("OpenTelemetry tracing is enabled and configured.")
-
-    FlaskInstrumentor().instrument_app(app)
-    RequestsInstrumentor().instrument()
+    # Add the OTLP exporter to the tracer provider
+    otlp_span_processor = BatchSpanProcessor(otlp_exporter)
+    trace_provider.add_span_processor(otlp_span_processor)
